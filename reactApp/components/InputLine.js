bb45f3f70e63ba661e9456060c7d3bafcc6c3ec7
git push -u origin <barbara.ogyasi@gmail.com>
2019-07-22 16:34:17 -0400

    implemented adding todos with typed text

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -5,14 +5,22 @@ import ReactDOM from 'react-dom';
 class InputLine extends React.Component {
     constructor(props){
         super(props);
-        this.state = { 
+        this.state = {typedText:''
         };
+    }
+
+    handleTyping(event){
+        this.setState({typedText:event.target.value})
+    }
+    handleSubmit(){
+        this.props.submit(this.state.typedText)
+        this.setState({typedText:''})
     }
     render(){
         return(
             <span>
-                <input type="text"/>
-                <button onClick={()=>this.props.submit("Test Task")}> Add Todo</button>
+                <input type="text" onChange={(event)=>this.handleTyping(event)} value={this.state.typedText}/>
+                <button onClick={()=>this.handleSubmit()}> Add Todo</button>
             </span>
         )
     }
