80601f02628773286a8fcedcf057a6b27777d205
git push -u origin <barbara.ogyasi@gmail.com>
2019-07-22 14:59:13 -0400

    basic UI

--- reactApp/app.js
+++ reactApp/app.js
@@ -5,6 +5,42 @@
 // const ReactDOM = require('react-dom');
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import Todo from '../../react-todo-codealong-7-22/reactApp/components/Todo';
 
-ReactDOM.render(<p>React lives!</p>,
+let dummyData = ['Make dinner', 'Go to the gym', 'Take over the world ']
+
+class Todo extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+        };
+        }
+    render(){
+        return(
+            <li>
+                <span>
+                <button> X </button>
+                {this.props.task}
+                </span>
+            </li>
+        )
+    }    
+}
+
+class TodoList extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+        };
+    }
+
+    render() {
+      return(
+            <ul> {dummyData.map((taskName)=> <Todo
+            task={taskName}/>)} </ul>
+      )}
+  }
+
+
+ReactDOM.render(<TodoList />,
     document.getElementById('root'));
